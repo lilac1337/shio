2ef8893414619db0acd23b12dcd3eaa67dc88500
lilac1337 <pinkknight1@protonmail.com>
2026-07-03 15:24:04 -0400

    add c keywords

--- src/filetypes.c
+++ src/filetypes.c
@@ -3,9 +3,14 @@
 const char *c_hl_ext[] = { ".c", ".h", ".cpp", ".cxx", "hxx", ".hpp", NULL };
 const char *c_hl_kws[] = { "switch", "if", "while", "for", "break", "continue", "return",
                            "else", "struct", "union", "typedef", "static", "enum", "case",
+                           "const", "constexpr", "default", "extern", "false", "true", "inline",
+                           "goto", "nullptr", "register", "restrict", "sizeof", "static",
+                           "static_assert", "thread_local", "typeof", "typeof_unequal",
+                           "volatile",
                            
                            "int|", "long|", "double|", "float|", "char|", "unsigned|", "signed|",
-                           "void|", NULL };
+                           "void|", "bool|", "auto|", "_Atomic|", "_Complex|", "_Decimal32|",
+                           "_Decimal64|", "_Decimal128|", "_Generic|", "_Imaginary|", NULL };
 
 syntax hldb[] = {
     { "c", c_hl_ext, c_hl_kws, "#", "//", "/*", "*/", HLF_NUMBERS | HLF_STRINGS },
